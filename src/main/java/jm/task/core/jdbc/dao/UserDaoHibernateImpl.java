86115d1798f0d221ca977ece376e317d6505cdf4
Leonid <leobalashiv@gmail.com>
2024-12-03 21:02:58 +0500

    выполнение задачи 1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,27 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
+
 public class UserDaoHibernateImpl implements UserDao {
+
+    private final String DB_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS users(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(64), lastName VARCHAR(64), age TINYINT(3))";
+    private final String DB_DROP_TABLE = "DROP TABLE IF EXISTS users";
+    private final String DB_CLEAN_USERS = "DELETE FROM users";
+
+
+    SessionFactory sessionFactory = Util.getSessionFactory();
+    Session session = sessionFactory.getCurrentSession();
+    Transaction transaction;
     public UserDaoHibernateImpl() {
 
     }
@@ -12,31 +29,94 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        try (Session session = sessionFactory.getCurrentSession()) {
+            transaction = session.beginTransaction();
+            session.createNativeQuery(DB_CREATE_TABLE).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Throwable e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = sessionFactory.getCurrentSession()) {
+            transaction = session.beginTransaction();
+            session.createNativeQuery(DB_DROP_TABLE).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Throwable e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        try {
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
 
+            User user = new User(name, lastName, age);
+            session.save(user);
+            System.out.println("User с именем " + name + " добавлен в базу данных");
+
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try {
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
 
+            User user = session.get(User.class, id);
+            session.delete(user);
+
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+
+        session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        Query q = session.createQuery("From User");
+
+        List<User> resultList = q.list();
+        try {
+
+            System.out.println("num of user:" + resultList.size());
+
+            for (User next : resultList) {
+                System.out.println("next user: " + next);
+            }
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
+        return resultList;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = sessionFactory.getCurrentSession()) {
+            transaction = session.beginTransaction();
+            session.createNativeQuery(DB_CLEAN_USERS).executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("База данных очищена.");
+        } catch (Throwable e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import javax.imageio.spi.ServiceRegistry;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
+
     private static final String dbURL = "jdbc:mysql://localhost:3306/mydbtest";
     private static final String dbUserName = "root";
     private static final String dbPassword = "root";
@@ -20,4 +29,40 @@ public class Util {
         }
         return connection;
     }
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = getConfiguration();
+
+                sessionFactory = configuration.buildSessionFactory();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+    private static Configuration getConfiguration() {
+        Configuration configuration = new Configuration();
+
+        Properties settings = new Properties();
+        settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        settings.put(Environment.URL, dbURL);
+        settings.put(Environment.USER, dbUserName);
+        settings.put(Environment.PASS, dbPassword);
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+
+        settings.put(Environment.SHOW_SQL, "true");
+
+        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+        settings.put(Environment.HBM2DDL_AUTO, "");
+        configuration.addAnnotatedClass(User.class);
+        configuration.setProperties(settings);
+        return configuration;
+    }
 }
+
